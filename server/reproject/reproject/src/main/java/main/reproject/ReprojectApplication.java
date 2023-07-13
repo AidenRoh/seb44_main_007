e4868f08bce97d7d9a6babcccebb25b70bdf0319
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-07-13 16:37:39 +0900

    feature/start

---
+++ server/reproject/reproject/src/main/java/main/reproject/ReprojectApplication.java
@@ -0,0 +1,13 @@
+package main.reproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReprojectApplication.class, args);
+	}
+
+}
---
+++ server/reproject/reproject/src/test/java/main/reproject/ReprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package main.reproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
